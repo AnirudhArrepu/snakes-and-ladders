8eb6101c46b726d20d8db815aaae6a3dd1bf3c71
AnirudhArrepu <anirudharrepu@gmail.com>
2024-11-02 23:08:53 +0530

    major changes

--- snakes-and-ladders/client/src/App.js
+++ snakes-and-ladders/client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import io from 'socket.io-client';
 import Grid from './components/grid';
 import Dice from './components/dice';
+import Papa from 'papaparse';
 import ShowGIF from './components/showGIF';
 
 const socket = io.connect('http://localhost:3001')
@@ -16,21 +17,64 @@ function App() {
   // const [myIndex, setMyIndex] = useState(0);
   const [turnIndex, setTurnIndex] = useState(0);
   const [winner, setWinner] = useState('');
-
-  let gifs = {
-    1: 'https://miro.medium.com/v2/resize:fit:640/format:webp/1*AmI9wRbXrfIWGESx6eEiTw.gif',
-    2: 'https://miro.medium.com/v2/resize:fit:640/format:webp/1*AmI9wRbXrfIWGESx6eEiTw.gif',
-    3: 'https://www.google.com/imgres?q=telugu%20gif%20about%20karma&imgurl=https%3A%2F%2Fmedia.tenor.com%2FbAkbr_b46GYAAAAM%2Fnaa-karma-na-karma.gif&imgrefurl=https%3A%2F%2Ftenor.com%2Fview%2Fkarma-ra-babu-sticker-thala-kottukovadam-cha-my-karma-gif-19099055&docid=WC5gyHoZGChb1M&tbnid=lgz2lNG1UjriFM&vet=12ahUKEwjF1KvxrO-IAxUGiK8BHYi4Nn0QM3oECGcQAA..i&w=220&h=125&hcb=2&ved=2ahUKEwjF1KvxrO-IAxUGiK8BHYi4Nn0QM3oECGcQAA',
-    4: 'https://miro.medium.com/v2/resize:fit:640/format:webp/1*AmI9wRbXrfIWGESx6eEiTw.gif',
-    5: 'https://miro.medium.com/v2/resize:fit:640/format:webp/1*AmI9wRbXrfIWGESx6eEiTw.gif',
-    6: 'https://miro.medium.com/v2/resize:fit:640/format:webp/1*AmI9wRbXrfIWGESx6eEiTw.gif',
-    7: 'https://miro.medium.com/v2/resize:fit:640/format:webp/1*AmI9wRbXrfIWGESx6eEiTw.gif',
-    8: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.pinterest.com%2Fpin%2Ftelugu-brahmi-gif-telugu-brahmi-telugucomedy-discover-share-gifs--694328467529047753%2F&psig=AOvVaw09m9ByqaommIAak0aEoRP7&ust=1727946830503000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKj14Nyt74gDFQAAAAAdAAAAABAw',
-    9: 'https://miro.medium.com/v2/resize:fit:640/format:webp/1*AmI9wRbXrfIWGESx6eEiTw.gif',
-    10:'https://miro.medium.com/v2/resize:fit:640/format:webp/1*AmI9wRbXrfIWGESx6eEiTw.gif',
-  }
+  const [snakesData, setSnakesData] = useState([[]]);
+  const [laddersData, setLaddersData] = useState([[]]);
+  const [cellData, setCellData] = useState([[]]);
+  const [chosenLanguage, setChosenLanguage] = useState(0);
   
   useEffect(() => {
+    //loading snakes.csv
+    fetch("/snakes.csv")
+      .then((response) => response.text())
+      .then((csvText) => {
+        Papa.parse(csvText, {
+          header: true,
+          dynamicTyping: true,
+          complete: (result) => {
+            setSnakesData((prevData) => [
+              ...prevData,
+              ...result.data.map((row) => Object.values(row))
+            ]);
+          },
+        });
+      }
+    );
+    fetch("/ladders.csv")
+      .then((response) => response.text())
+      .then((csvText) => {
+        Papa.parse(csvText, {
+          header: true,
+          dynamicTyping: true,
+          complete: (result) => {
+            setLaddersData((prevData) => [
+              ...prevData,
+              ...result.data.map((row) => Object.values(row))
+            ]);
+          },
+        });
+      }
+    );
+    fetch("/cells.csv")
+      .then((response) => response.text())
+      .then((csvText) => {
+        Papa.parse(csvText, {
+          header: true,
+          dynamicTyping: true,
+          complete: (result) => {
+            setCellData((prevData) => [
+              ...prevData,
+              ...result.data.map((row) => Object.values(row))              
+            ]);
+          },
+        });
+      }
+    );
+
+    console.log(cellData);
+    
+
+
+
     socket.on('players_update', (playersData) => {
         setPlayers(playersData);
         // setMyIndex(players.length -1);
@@ -73,18 +117,25 @@ function App() {
           <input type='text' placeholder='Name' onChange={(event)=>{setUsername(event.target.value)}}/>
           <input type='text' placeholder='Room'onChange={(event)=>{setRoom(event.target.value)}}/>
           <input type='color' className= "colorbutton" placeholder='Choose color' onChange={(event)=>{setColor(event.target.value)}}/>
+          <select name="Choose language" onChange={(e) => setChosenLanguage(e.target.value)}>
+            <option value="0">Telugu</option>
+            <option value="1">Malayalam</option>
+            <option value="2">Tamil</option>
+            <option value="3">Hindi</option>
+          </select>
           <button className="joinbutton" onClick={joinRoom}>Join</button>
+
         </div>)
         : (
           <div className='content'>
 
             <div className='content left'>
-              <Grid players={players}/>
+              <Grid players={players} snakes={snakesData} ladders={laddersData} cellData={cellData[chosenLanguage+1]}/>
             </div>
 
             <div className='content right'>
               <div className='showgif'>
-                <ShowGIF players={players} username={username} gifs={gifs}/>
+                {/* <ShowGIF players={players} username={username} gifs={gifs}/> */}
               </div>
               <div className='showdice'>
                 <Dice players={players} turnIndex={turnIndex} socket={socket} winner={winner} room={room}/>
